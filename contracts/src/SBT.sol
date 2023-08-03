8a2290e3f939c9a79f6053a8b17264c6c35cb288
Max Korolev <nrjshka@gmail.com>
2023-08-03 18:51:06 +0100

    added: cleaned contracts

---
+++ contracts/src/SBT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract SBT is ERC721 {
+    constructor() ERC721("SBT", "SBT") {}
+}
