467f44df3a138dc244e8326554b981797898328c
Max Korolev <nrjshka@gmail.com>
2023-08-04 23:01:55 +0100

    added: sbt votable update

--- contracts/src/SBT.sol
+++ contracts/src/SBT.sol
@@ -2,8 +2,21 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 
 
-contract SBT is ERC721 {
+contract SBT is ERC721, ERC721Votes {
     constructor() ERC721("SBT", "SBT") {}
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
 }
