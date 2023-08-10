5575de846bf6c483fe7bdd028b33fae5267ac8e3
Max Korolev <nrjshka@gmail.com>
2023-08-10 11:51:28 +0100

    added; updates for button classes

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 function Button({ children }: PropsWithChildren) {
   return (
-    <button className="h-14 w-full rounded-[20px] bg-buttonGradient font-semibold text-white/80">
+    <button className="h-14 w-full rounded-[20px] bg-buttonGradient font-semibold text-white/80 transition-all hover:opacity-90 active:opacity-70">
       {children}
     </button>
   );
