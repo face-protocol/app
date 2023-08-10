ece843c8c19ef35c18b8c15c2a1e873d242d80ee
Max Korolev <nrjshka@gmail.com>
2023-08-10 09:36:42 +0100

    cleaned unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
-
-import { Attestooooooor } from "./components";
-import { GoogleLogin } from "@react-oauth/google";
-
 export function App() {
-  return (
-    <main>
-      <GoogleLogin onSuccess={() => alert("Success!")} />
-    </main>
-  );
+  return <main></main>;
 }
