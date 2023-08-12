acb22ab13c60cfa7612007628ee2624e87d24e12
Max Korolev <nrjshka@gmail.com>
2023-08-12 15:21:06 +0100

    added: factory contract

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,9 @@ const CONTRACTS: TContracts = {
   COMMUNITY: {
     [optimismGoerli.id]: "0x1f6fe24d1ae85d66cfc9391176163fd2bf495d73",
   },
+  FACTORY: {
+    [optimismGoerli.id]: "0x963a68D8a57E8E863Cd4E6411B7321988280465f",
+  },
 };
 
 export { DEFAULT_CHAIN_ID, CONTRACTS, optimismGoerli };
